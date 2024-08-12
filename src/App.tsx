6eb7934d3d489ad56a5c2304cdd568963242a188
oscar.jigbring <oscar.jigbring@hotmail.se>
2024-08-12 21:44:19 +0200

    Started adding context.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
+import { Context } from "./PickContext";
 
 function App() {
+  const [context, setContext] = useState("poop");
   return (
     <>
-      <Header />
+      <Context.Provider value={[context, setContext]}>
+        <Header />
+      </Context.Provider>
     </>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,24 @@
+import { useContext } from "react";
+import { Context } from "../PickContext";
+
 export default function Header() {
+  const [context, setContext] = useContext(Context);
   return (
     <header className="flex flex-col">
-      <h1 className="text-5xl">Calculator</h1>
-      <div className="flex text-2xl">
+      <h1 className="text-5xl py-3">Calculator</h1>
+      {context === "poop" ? <h2>TJENA</h2> : null}
+      <div className="flex text-xl lg:text-3xl overflow-x-auto snap-x">
         <div className="border p-5 w-full text-center hover:bg-black transition-all hover:text-white hover:cursor-pointer">
           Percentage
         </div>
         <div className="border p-5 w-full text-center hover:bg-black transition-all hover:text-white hover:cursor-pointer">
-          Percentage
+          BMI
         </div>
         <div className="border p-5 w-full text-center hover:bg-black transition-all hover:text-white hover:cursor-pointer">
-          Percentage
+          +
+        </div>
+        <div className="border p-5 w-full text-center hover:bg-black transition-all hover:text-white hover:cursor-pointer">
+          -
         </div>
       </div>
     </header>
